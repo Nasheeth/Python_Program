d4e0e6c30391190c45f2e73231935f0fdb989373
Nasheeth <nasheethnash@gmail.com>
2021-12-23 22:57:32 +0530

    test

---
+++ Week2/Use a for statement to print 10 random numbers.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(0, 10):
+    a = random.randrange(25, 35)
+    print(a)
---
+++ Week2/hypotenuse of a right triangle.py
@@ -0,0 +1,8 @@
+import math
+
+a = int(input("Enter the length of side (in cm) a = "))
+b = int(input("Enter the length of side (in cm) b = "))
+
+Hypotenuse = math.hypot(a, b)
+
+print(Hypotenuse)
