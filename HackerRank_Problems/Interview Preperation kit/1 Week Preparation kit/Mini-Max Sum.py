642cf4800823f783a4b39a2a5e9be69f8eff7b08
Nasheeth <nasheethnash@gmail.com>
2022-01-12 13:21:28 +0530

    Mini-max

--- HackerRank_Problems/Interview Preperation kit/1 Week Preparation kit/Mini-Max Sum.py
+++ HackerRank_Problems/Interview Preperation kit/1 Week Preparation kit/Mini-Max Sum.py
@@ -2,3 +2,22 @@
 # summing exactly four of the five integers. Then print the respective minimum and maximum values as
 # a single line of two space-separated long integers.
 
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    s = sum(arr)
+    max_val = s - arr[0]
+    min_val = s - arr[-1]
+    print(min_val, max_val)
+
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
